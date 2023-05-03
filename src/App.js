d606594bc723802a31d03fea765481550601198e
Najim Uddin <najim7304@gmail.com>
2023-05-04 00:35:28 +0600

    router-provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import { createBrowserRouter, Routes, Route, NavLink, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+// LAYOUTS 
+import RootLayout from "./layouts/RootLayout";
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Routing</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to='about'>About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    )
+  )
+  return < RouterProvider router={router} />
 }
 
 export default App;
