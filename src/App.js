a9a3b6def06a62157358a2b44e803ff9ad48db83
Najim Uddin <najim7304@gmail.com>
2023-05-04 00:05:56 +0600

    router basic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 function App() {
   return (
     <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Routing</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to='about'>About</NavLink>
+        </nav>
+      </header>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
         </Routes>
       </main>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,27 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <h2>Welcome Home</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        quasi repellendus culpa exercitationem, unde magni cupiditate qui earum,
+        ducimus minima quia possimus molestiae, numquam fugiat rem optio laborum
+        temporibus maiores?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque minus,
+        quis consequuntur, illum ducimus earum tempore, similique saepe adipisci
+        officiis excepturi facere a facilis eveniet culpa repellendus sed neque
+        in!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque facilis
+        labore rerum enim, voluptas voluptatem quam expedita natus minima
+        quaerat eligendi ab harum, sapiente aliquam molestias maxime nulla quos
+        odit?
+      </p>
+    </div>
+  );
 };
 
 export default Home;
